f57566d21df9b61ed8cd1d1b06c084ef27680715
Danny Yang <danny.yang@granicus.com>
2020-09-19 21:15:51 -0500

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,64 @@
+﻿using GuestBookLibrary.Models;
+using System;
+using System.Collections.Generic;
+
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        private static List<GuestModel> guests = new List<GuestModel>();
+
+        static void Main(string[] args)
+        {
+
+            GetGuestInformation();
+
+            PrintGuestInformation();
+
+            Console.ReadLine();
+        }
+
+        private static void GetGuestInformation()
+        {
+            // Capture the information about each guest (assumption is at least one guest and unknown maximum)
+            // Info to capture: First name, last name, message to the host
+            string moreGuestsComing = "";
+
+            do
+            {
+                GuestModel guest = new GuestModel();
+
+                guest.FirstName = GetInfoFromConsole("What is your first name: ");
+                guest.LastName = GetInfoFromConsole("What is your last name: ");
+                guest.MessageToHost = GetInfoFromConsole("What message would you like to tell your host: ");
+                moreGuestsComing = GetInfoFromConsole("Are more guests coming (yes/no): ");
+
+                guests.Add(guest);
+
+                Console.Clear();
+
+            } while (moreGuestsComing.ToLower() == "yes");
+        }
+
+        private static string GetInfoFromConsole(string message)
+        {
+            string output = "";
+
+            Console.Write(message);
+            output = Console.ReadLine();
+
+            return output;
+        }
+
+        private static void PrintGuestInformation()
+        {
+            // Once done, loop through each guest and print their info
+            foreach (GuestModel guest in guests)
+            {
+                Console.WriteLine(guest.GuestInfo);
+            }
+
+        }
+    }
+}
---
+++ GuestBookLibrary/Models/GuestModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GuestBookLibrary.Models;
+
+namespace GuestBookLibrary.Models
+{
+    public class GuestModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MessageToHost { get; set; }
+
+        public string GuestInfo
+        {
+            get 
+            { 
+                return $"{FirstName} {LastName}: {MessageToHost}"; 
+            }
+        }
+
+    }
+}
